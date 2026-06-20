cb3a9ef2d4ae9a3b5437f16f379a444b39ee61a0
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2026-06-21 00:50:02 +0300

    feat: integrate Scalar API documentation and enable controllers in the application pipeline

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using DotnetPN.Data;
 using DotnetPN.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 // Add EF Core DbContext with SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -19,8 +21,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("DotnetPN API Documentation")
+            .WithTheme(ScalarTheme.DeepSpace)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
+
 app.Run();
+
